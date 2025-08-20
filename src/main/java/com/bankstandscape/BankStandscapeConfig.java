ac77e1361354195e87a2f6022416d8ccf4c2cc31
Addison Mikkelson <amarshallmikkelson@gmail.com>
2025-08-20 16:58:06 -0500

    Simplify to exact RuneLite example structure to fix packager reading errors

--- src/main/java/com/bankstandscape/BankStandscapeConfig.java
+++ src/main/java/com/bankstandscape/BankStandscapeConfig.java
@@ -8,42 +8,12 @@ import net.runelite.client.config.ConfigItem;
 public interface BankStandscapeConfig extends Config
 {
 	@ConfigItem(
-		keyName = "enablePlugin",
-		name = "Enable Plugin",
-		description = "Enables or disables the Bank Standscape plugin"
+		keyName = "greeting",
+		name = "Welcome Greeting",
+		description = "The message to show to the user when they login"
 	)
-	default boolean enablePlugin()
+	default String greeting()
 	{
-		return true;
-	}
-
-	@ConfigItem(
-		keyName = "enableXpScaling",
-		name = "XP Scaling",
-		description = "Increases XP per minute as your level increases"
-	)
-	default boolean enableXpScaling()
-	{
-		return true;
-	}
-
-	@ConfigItem(
-		keyName = "enableMultiBank",
-		name = "Multi-Bank XP",
-		description = "Allows gaining XP at other major banks (Grand Exchange is always enabled)"
-	)
-	default boolean enableMultiBank()
-	{
-		return false;
-	}
-
-	@ConfigItem(
-		keyName = "showOverlay",
-		name = "Show Overlay",
-		description = "Shows the Bank Standscape overlay with level and XP information"
-	)
-	default boolean showOverlay()
-	{
-		return true;
+		return "Hello";
 	}
 }
--- src/main/java/com/bankstandscape/BankStandscapePlugin.java
+++ src/main/java/com/bankstandscape/BankStandscapePlugin.java
@@ -3,9 +3,10 @@ package com.bankstandscape;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.*;
-import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.events.*;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -13,9 +14,7 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Bank Standscape",
-	description = "Gain XP for standing idle at banks.",
-	tags = {"bank", "stand", "afk", "idle", "xp"}
+	name = "Bank Standscape"
 )
 public class BankStandscapePlugin extends Plugin
 {
@@ -25,20 +24,10 @@ public class BankStandscapePlugin extends Plugin
 	@Inject
 	private BankStandscapeConfig config;
 
-	private long totalXp = 0;
-	private int currentLevel = 1;
-	private int idleTicksCounter = 0;
-	private WorldPoint lastPosition;
-
 	@Override
 	protected void startUp() throws Exception
 	{
 		log.info("Bank Standscape started!");
-		if (client.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", 
-				"Bank Standscape Plugin enabled!", null);
-		}
 	}
 
 	@Override
@@ -48,65 +37,12 @@ public class BankStandscapePlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameTick(GameTick gameTick)
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (client.getGameState() != GameState.LOGGED_IN)
+		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
-			return;
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Bank Standscape Plugin enabled!", null);
 		}
-
-		Player localPlayer = client.getLocalPlayer();
-		if (localPlayer == null)
-		{
-			return;
-		}
-
-		WorldPoint currentPosition = localPlayer.getWorldLocation();
-		boolean inBank = isPlayerInBankArea(currentPosition);
-		boolean isIdle = isPlayerIdle(localPlayer, currentPosition);
-
-		if (inBank && isIdle)
-		{
-			idleTicksCounter++;
-			if (idleTicksCounter >= 100) // 60 seconds
-			{
-				grantXp();
-				idleTicksCounter = 0;
-			}
-		}
-		else
-		{
-			idleTicksCounter = 0;
-		}
-
-		lastPosition = currentPosition;
-	}
-
-	private boolean isPlayerInBankArea(WorldPoint location)
-	{
-		if (location == null) return false;
-		
-		// Grand Exchange area
-		return location.getX() >= 3149 && location.getX() <= 3180 && 
-			   location.getY() >= 3501 && location.getY() <= 3524 && 
-			   location.getPlane() == 0;
-	}
-
-	private boolean isPlayerIdle(Player player, WorldPoint currentPosition)
-	{
-		if (lastPosition == null || !currentPosition.equals(lastPosition))
-		{
-			return false;
-		}
-		
-		return player.getAnimation() == -1;
-	}
-
-	private void grantXp()
-	{
-		totalXp += 10;
-		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", 
-			"You gain 10 Bank Standscape XP! (Total: " + totalXp + ")", null);
 	}
 
 	@Provides
